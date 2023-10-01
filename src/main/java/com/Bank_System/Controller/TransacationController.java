c1ba2aae2b4d95ad70e2f464102954b8f0ba3416
harshmunjapara09 <harshmunjapara005@gmail.com>
2023-10-01 11:08:41 +0530

    01-10-2023

--- src/main/java/com/Bank_System/Controller/TransacationController.java
+++ src/main/java/com/Bank_System/Controller/TransacationController.java
@@ -2,6 +2,7 @@ package com.Bank_System.Controller;
 
 import com.Bank_System.Model.Bank;
 import com.Bank_System.Model.TransactionHistory;
+import com.Bank_System.Service.EmailService;
 import com.Bank_System.Service.TranscationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,9 @@ public class TransacationController {
     @Autowired
     TranscationService transcationService;
 
+    @Autowired
+    EmailService emailService;
+
     @PostMapping("/add")
     public TransactionHistory add(TransactionHistory transactionHistory){
         return transcationService.add(transactionHistory);
@@ -22,7 +26,10 @@ public class TransacationController {
 
     @GetMapping("/passbook")
     public List<TransactionHistory> getAll(@RequestParam Long accountNumber){
-        return transcationService.getAll(accountNumber);
+        List<TransactionHistory> list = transcationService.getAll(accountNumber);
+        String email = "harshmunjapara005@gmail.com";
+        emailService.sendTransactionHistoryEmail(email,list);
+        return list;
     }
 
 
